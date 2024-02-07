1193fd101122e8f70c398f19c82c626899676356
Igor Aguiar <igory14aguiar@gmail.com>
2024-02-07 13:57:40 -0300

    adicionar aula 3 e alterar cor para laranja

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ function verificarChute() {
 }
 
 
-let cores = [ 'azul', 'amarelo', 'verde', 'branco'];
+let cores = [ 'azul', 'laranja', 'verde', 'branco'];
 console.log(cores[cores.length -1]);
 console.log(cores[2])
